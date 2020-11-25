9644788895090cba088d9355a9c0e37d00e4280c
RoundofThree <zhuoyingjiangli@gmail.com>
2020-11-25 05:30:40 -0800

    Fix transaction controller tests

--- test/controllers/transactions_controller_test.rb
+++ test/controllers/transactions_controller_test.rb
@@ -7,10 +7,10 @@ class TransactionsControllerTest < ActionDispatch::IntegrationTest
   test "should not get show if not logged in" do
     transaction = transactions(:one)
     get transaction_url(transaction)
-    assert_redirected_to sign_in_url  # TODO: check routes!!!!!!!!!!!!!!!!!!
+    assert_redirected_to new_user_session_url
   end
 
-  test "should get show if transaction is done by user" do
+  test "should get show if transaction is done by logged in user" do
     user = users(:have_two_accounts)
     sign_in user
     transaction = transactions(:one)
@@ -18,8 +18,8 @@ class TransactionsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should not get show if transaction is not done by user" do
-    user = users(:have_one_accounts)
+  test "should not get show if transaction is not done by logged in user" do
+    user = users(:have_one_account)
     sign_in user
     transaction = transactions(:one)
     get transaction_url(transaction)
@@ -28,7 +28,7 @@ class TransactionsControllerTest < ActionDispatch::IntegrationTest
 
   # new tests
   test "should get new if user has an account" do
-    user = users(:have_one_accounts)
+    user = users(:have_one_account)
     sign_in user
     get new_transaction_url
     assert_response :success
@@ -43,24 +43,50 @@ class TransactionsControllerTest < ActionDispatch::IntegrationTest
 
   # create tests
 
+  test "create transaction if user has not logged in should fail" do
+    payer_account = accounts(:one)
+    transaction = Transaction.new(payer_account: payer_account, payee_account_number: "12345678901234", payee_fullname: "Nyx", amount: 1.0, currency: "£")
+    assert_no_difference 'Transaction.count'  do
+      post transactions_url, params: { transaction: transaction.attributes }
+    end
+    assert_redirected_to new_user_session_url
+  end
+
   test "create transaction with valid account_id, payee account number and amount should succeed" do
+    user = users(:have_two_accounts)
+    sign_in user
     payer_account = accounts(:one)
     transaction = Transaction.new(payer_account: payer_account, payee_account_number: "12345678901234", payee_fullname: "Nyx", amount: 1.0, currency: "£")
     assert_difference('Transaction.count', 1) do
       post transactions_url, params: { transaction: transaction.attributes }
     end
-    assert_redirected_to transactions_url
+    assert_redirected_to transaction_url(transaction.id)
     # maybe check the transaction is the same one created? But how in the earth do I get the ID of the created transaction...
   end
 
   test "create transaction without account_id should fail" do
+    user = users(:have_two_accounts)
+    sign_in user
     assert_no_difference 'Transaction.count' do
       post transactions_url, params: { transaction: { payer_account: "", payee_account_number: "12345678901234", payee_fullname: "Nyx", amount: 1.0, currency: "£" } }
     end
     assert_redirected_to new_transaction_url
   end
 
+  test "create transaction with an account_id that does not belong to logged in user should fail" do
+    user = users(:have_one_account)
+    sign_in user
+    payer_account = accounts(:one)
+    transaction = Transaction.new(payer_account: payer_account, payee_account_number: "12345678901234", payee_fullname: "Nyx", amount: 1.0, currency: "£")
+    assert_no_difference 'Transaction.count' do
+      post transactions_url, params: { transaction: transaction.attributes }
+    end
+    assert_redirected_to new_transaction_url
+  end
+
   test "create transaction with insufficient balance account should fail" do
+    user = users(:have_two_accounts)
+    sign_in user
     payer_account = accounts(:one)
     assert_no_difference 'Transaction.count' do
       post transactions_url, params: { transaction: { payer_account: payer_account, payee_account_number: "12345678901234", payee_fullname: "Nyx", amount: 1000000.0, currency: "£" } }
@@ -69,6 +95,8 @@ class TransactionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "create transaction without valid payee account number should fail" do
+    user = users(:have_two_accounts)
+    sign_in user
     payer_account = accounts(:one)
     assert_no_difference 'Transaction.count' do
       post transactions_url, params: { transaction: { payer_account: payer_account, payee_account_number: "1234", payee_fullname: "Nyx", amount: 1000000.0, currency: "£" } }
