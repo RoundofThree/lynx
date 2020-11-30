d5131c1fe3fd62bd74cf782301d905606e53d7f3
Naissy <helen_hong_atlancia@outlook.com>
2020-11-30 23:34:41 +0800

    update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jqurey").start()
+require("popper").start()
+require("boostrap").start()
 
 import "bootstrap"
 document.addEventListener("turbolinks:load", ()=> {
@@ -14,10 +17,12 @@ document.addEventListener("turbolinks:load", ()=> {
  $('[data-toggle="tooltip"]').popover()
 
 })
-import "../stylesheets/home"
+import "../stylesheets/application"
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+//= require jquery
+//= require bootstrap-sprockets
