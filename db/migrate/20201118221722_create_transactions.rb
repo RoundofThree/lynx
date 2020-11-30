ad56f117195a976f101b1cda8f96b52e9180519d
HonestBook <lsmwallfacer@gmail.com>
2020-11-30 22:15:29 +0800

    Revert changes in database schema

--- db/migrate/20201118221722_create_transactions.rb
+++ db/migrate/20201118221722_create_transactions.rb
@@ -1,7 +1,7 @@
 class CreateTransactions < ActiveRecord::Migration[6.0]
   def change
     create_table :transactions do |t|
-      t.references :account, null: false, foreign_key: true
+      t.references :payer_account, null: false
       t.decimal :amount, null: false
       t.string :currency, null: false
       t.string :payee_account_number, null: false
@@ -11,7 +11,7 @@ class CreateTransactions < ActiveRecord::Migration[6.0]
       t.timestamps
     end
 
-    # add_foreign_key :transactions, :accounts, column: :payer_account_id
+    add_foreign_key :transactions, :accounts, column: :payer_account_id
     # add_foreign_key :transactions, :accounts, column: :payee_account_id
   end
 end
