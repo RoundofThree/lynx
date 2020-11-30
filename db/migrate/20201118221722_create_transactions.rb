d684b4949a92cc0be18b9bd59c9188907ffa70ad
HonestBook <lsmwallfacer@gmail.com>
2020-11-30 19:12:56 +0800

    Modify transaction schema

--- db/migrate/20201118221722_create_transactions.rb
+++ db/migrate/20201118221722_create_transactions.rb
@@ -1,9 +1,9 @@
 class CreateTransactions < ActiveRecord::Migration[6.0]
   def change
     create_table :transactions do |t|
+      t.references :account, null: false, foreign_key: true
       t.decimal :amount, null: false
       t.string :currency, null: false
-      t.references :payer_account, null: false
       t.string :payee_account_number, null: false
       t.string :payee_fullname, null: false
       t.string :reference  # kinda why you are tranfering 
@@ -11,7 +11,7 @@ class CreateTransactions < ActiveRecord::Migration[6.0]
       t.timestamps
     end
 
-    add_foreign_key :transactions, :accounts, column: :payer_account_id
+    # add_foreign_key :transactions, :accounts, column: :payer_account_id
     # add_foreign_key :transactions, :accounts, column: :payee_account_id
   end
 end
