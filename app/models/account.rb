94f950e53a62ed63fbca8196520e5530f3fc94a7
HonestBook <lsmwallfacer@gmail.com>
2020-11-30 18:53:42 +0800

    Create database relations

--- app/models/account.rb
+++ app/models/account.rb
@@ -7,7 +7,8 @@ class Account < ApplicationRecord
   before_validation :check_balance
 
   belongs_to :user
-
+  
+  has_many :transactions
   private
     def check_balance
       if self.new_record?
