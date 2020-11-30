383e991be0187e412a0e00f59fe0379d5e8ddb35
RoundofThree <zhuoyingjiangli@gmail.com>
2020-11-30 22:32:49 +0100

    Restore restriction to home page for logged in users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   # no need to login
   skip_before_action :authenticate_user!
-  # before_action :redirect_logged_user, if: -> { current_user.present? }
+  before_action :redirect_logged_user, if: -> { current_user.present? }
 
   def index
   end
