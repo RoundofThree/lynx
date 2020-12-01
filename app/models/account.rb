df098365a0dd2b0977af2994c0497bd48af3b901
HonestBook <lsmwallfacer@gmail.com>
2020-12-01 23:15:06 +0800

    Add new validations for account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,12 +1,17 @@
 class Account < ApplicationRecord
+  # A user has many accounts
+  belongs_to :user
+
   # validations
   validates :user, presence: true
   validates :account_number, presence: true, uniqueness: true
   validates :balance, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  # cvv should only be 3-digit or 4-digit long
+  validates :cvv, presense: true, length: {greater_than_or_equal_to: 3, less_than_or_equal_to: 4}
 
-  before_validation :check_balance
-  belongs_to :user
-  
+  # before_validation :check_balance
+
+
   has_many :transactions
   private
 
