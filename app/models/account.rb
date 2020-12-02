1d0d1732dac9c5c0eafd780997cbbe17f2023655
HonestBook <lsmwallfacer@gmail.com>
2020-12-02 20:17:01 +0800

    Simplify expiry_date representation

--- app/models/account.rb
+++ app/models/account.rb
@@ -14,11 +14,11 @@ class Account < ApplicationRecord
 
   def generate_expiry_date 
     not_rounded_date = Time.now + 4.years
-    self.expiry_date = Time.new(not_rounded_date.year, not_rounded_date.month)
+    self.expiry_date = Time.new(not_rounded_date.year, not_rounded_date.month, -1) # The last day of that month
   end 
 
   def not_expired 
-    if expiry_date.present? and (expiry_date.year >= Time.now.year or (expiry_date.year == Time.now.year and expiry_date.month >= Time.now.month))
+    if (expiry_date > Time.now)
       true
     else
       errors.add(:expiry_date, "Account is expired")
