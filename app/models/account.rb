57b48315cc17f1d1bb7961d490e09e5328773389
RoundofThree <zhuoyingjiangli@gmail.com>
2020-12-02 11:27:52 +0100

    Change expiry_date in database as datetime field

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,18 +1,34 @@
 class Account < ApplicationRecord
+  before_validation :check_balance
   # validations
   validates :user, presence: true
   validates :account_number, presence: true, uniqueness: true
   validates :balance, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :expiry_date, presence: true 
+  validate :not_expired 
 
-  before_validation :check_balance
+  before_create :generate_expiry_date
   belongs_to :user
 
   private
 
-    def check_balance
-      if self.new_record?
-        self.balance = 0.00
-      end
+  def generate_expiry_date 
+    not_rounded_date = Time.now + 4.years
+    self.expiry_date = Time.new(not_rounded_date.year, not_rounded_date.month)
+  end 
+
+  def not_expired 
+    if expiry_date.present? and (expiry_date.year >= Time.now.year or (expiry_date.year == Time.now.year and expiry_date.month >= Time.now.month))
+      true
+    else
+      errors.add(:expiry_date, "Account is expired")
+    end 
+  end  
+
+  def check_balance
+    if self.new_record?
+      self.balance = 0.00
     end
+  end
 
 end
