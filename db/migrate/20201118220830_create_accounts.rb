2946b71704cd989d987ec7a2133753a81d60e3de
HonestBook <lsmwallfacer@gmail.com>
2020-12-02 19:51:58 +0800

    Modify account schema of balance column
    The balance should now has two decimal places.

--- db/migrate/20201118220830_create_accounts.rb
+++ db/migrate/20201118220830_create_accounts.rb
@@ -2,7 +2,8 @@ class CreateAccounts < ActiveRecord::Migration[6.0]
   def change
     create_table :accounts do |t|
       t.references :user, null: false, foreign_key: true
-      t.decimal :balance
+      # the balance should always have 2 decimal places
+      t.decimal :balance, precision: 20, scale: 2 
       t.string :account_number
       t.string :cvv
       t.integer :expiry_month
