14820bd74d5ef412e7b6b18c08da4735cdb6cf05
RoundofThree <zhuoyingjiangli@gmail.com>
2020-12-03 11:57:53 +0100

    Fix home controller tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,14 +1,17 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   test "should remain at homepage if not logged in"do
     get root_url
     assert_response :success
   end
 
   test "should direct to personal dashboard if logged in" do
-    get dashboard_url
-    assert_response :success
-    sign_in :user
+    sign_in users(:have_one_account)
+    get root_url
+    assert_redirected_to dashboard_url
+    sign_out :user
   end
 end
