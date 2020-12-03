00d8a871f3437709fb63df0541f2e896575894a5
Naissy <helen_hong_atlancia@outlook.com>
2020-12-03 17:38:26 +0800

    Bug fi

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test "should direct to personal dashboard if logged in" do
+    get dashboard_url
+    assert_response :success
+    sign_in :user
+  end
+
+    test "should remain at homepage if not logged in" do
+    get root_url
+    assert_response :success
+  end
+
 end
