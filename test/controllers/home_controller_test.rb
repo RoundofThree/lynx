a9ce57e83927bc0b1174257742430add060d5037
HonestBook <lsmwallfacer@gmail.com>
2020-12-03 19:15:06 +0800

    Revert "Merge pull request #12 from RoundofThree/homepage"
    This reverts commit 84e0fadcfcaeea76648ef975cea9e673f7c145e0, reversing
    changes made to bfe5d26837fac229a7128c2c758aadf9cd329563.

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,14 +1,9 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should remain at homepage if not logged in"do
+  test "should get index" do
     get root_url
     assert_response :success
   end
 
-  test "should direct to personal dashboard if logged in" do
-    get dashboard_url
-    assert_response :success
-    sign_in :user
-  end
 end
