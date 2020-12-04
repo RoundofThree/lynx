bc5779a2e0269d6ddd2256edadfd95b10ad881db
HTTOM <k1926372@kcl.ac.uk>
2020-12-04 11:01:18 +0800

    add basic structure of user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,40 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "the name of user should be upcase after create" do
+  end
+
+  test "user should have 0 balance we after create" do
+  end
+
+  test "user should contain 2 accounts when we use user with 2 accounts" do
+  end
+
+  test "user whose age is smaller than 18 is invalid" do
+  end
+
+  test "user without firstname is invalid" do
+
+  end
+
+  test "user without lastname is invalid" do
+
+  end
+
+  test "user without email is invalid" do
+
+  end
+
+  test "user without invalid email should not pass validation" do
+
+  end
+
+  test "user without birth date is invalid" do
+
+  end
+
+  test "user without phone is invalid" do
+    
+  end
+
 end
