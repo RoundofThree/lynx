199ceb988db4ff98180245c6c2c44f3b45babe11
HonestBook <lsmwallfacer@gmail.com>
2020-12-09 18:31:40 +0800

    Fix indentation

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -58,13 +58,14 @@ class Admin::UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:all)
-    end
+  # Only allow a list of trusted parameters through.
+  def user_params
+     params.require(:user).permit(:all)
+  end
+  
 end
