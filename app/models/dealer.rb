cb9a08f6d8a0e654dc5001abdcf708b258db48e9
HonestBook <lsmwallfacer@gmail.com>
2020-12-11 02:35:37 +0800

    Add basic tests to dealer

--- app/models/dealer.rb
+++ app/models/dealer.rb
@@ -1,5 +1,10 @@
 class Dealer < ApplicationRecord
-
+  validates :name, presence: true
+  validates :account_number, presence: true, length: { is:14 }
+  validates :min_amount, presence: true
+  validates :max_amount, presence: true
+  validates :max_amount, numericality: { greater_than_or_equal_to: :min_amount }
+  validates :frequency, numericality: { less_than_or_equal_to: 300 }
   private
 
 end
--- test/models/dealer_test.rb
+++ test/models/dealer_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class DealerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "a dealer with proper attributes should be valid" do
+    dealer = Dealer.new(currency: "32", name: "Burger King", account_number: "12110987654321", min_amount: 2, max_amount: 20, frequency: 10)
+    assert dealer.valid?
+  end
+  
+  test "a dealer with max amount less than mix amount should be invalid" do
+    dealer = Dealer.new(currency: "32", name: "Burger King", account_number: "12110987654321", min_amount: 2, max_amount: 1, frequency: 10)
+    assert_not dealer.valid?
+  end
 end
