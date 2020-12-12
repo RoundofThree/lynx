2c39b16597ac6a1e29859855a5da34b4c15062e8
RoundofThree <zhuoyingjiangli@gmail.com>
2020-12-13 00:38:22 +0100

    Add support for markdown rendering

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
   include StyleHelper  # for custom style 
 
   protect_from_forgery with: :exception, prepend: true
