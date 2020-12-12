6e9faef5c4e4e3ddf337523966c28db1cc1087d1
RoundofThree <zhuoyingjiangli@gmail.com>
2020-12-12 15:06:36 +0100

    Add active storage for image upload

--- app/helpers/style_helper.rb
+++ app/helpers/style_helper.rb
@@ -1,9 +1,18 @@
 module StyleHelper
   def bank_name
     current_style&.bank_name.presence || "Lynx"
-  end 
+  end
+
+  # navbar_color
+  # dashboard_background_color
+  # home_background_color
+  # account ||
+  # login
+  # signin
+  # transaction
+  # payment
   
   def current_style 
     Style.first
-  end 
+  end
 end 
