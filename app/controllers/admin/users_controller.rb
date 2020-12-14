b794c201a655ffaff470342d5f979b5f389c626f
Naissy <helen_hong_atlancia@outlook.com>
2020-12-14 20:39:52 +0800

    correct redirect path

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -45,7 +45,7 @@ class Admin::UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to admin_users_path(@user), notice: 'User was successfully created.' }
+        format.html { redirect_to admin_users_url(@user), notice: 'User was successfully created.' }
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
@@ -58,7 +58,7 @@ class Admin::UsersController < ApplicationController
     respond_to do |format|
 
       if @user.update(user_params)
-        format.html { redirect_to admin_users_path(@user), notice: 'User was successfully updated.' }
+        format.html { redirect_to admin_users_url(@user), notice: 'User was successfully updated.' }
       else
         format.html { render :edit }
       end
