c6e9f86749ef442e1bb3fd974eaf8b1599146a77
HonestBook <lsmwallfacer@gmail.com>
2020-12-16 01:03:41 +0800

    Implement how_many and period options

--- app/controllers/admin/generator_controller.rb
+++ app/controllers/admin/generator_controller.rb
@@ -3,12 +3,13 @@ class Admin::GeneratorController < ApplicationController
   def generate_transactions
     accounts = params[:account].eql?("All") ? 
     Account.all : Account.where(:id => params[:account])
+    how_many = params[:how_many].to_i
     accounts.each do |account|
       matching_dealers = Dealer.where(:currency => account.currency)
-      for i in 0...10
+      for i in 0...how_many do 
         dealer = get_weighted_random_dealer matching_dealers
         amount = -(rand(dealer.max_amount) + dealer.min_amount)
-        time = rand((DateTime.now - 3.months)..DateTime.now)
+        time = get_random_time
         account.transactions.create!(
           account: account, 
           dealer_account_number: dealer.account_number,
@@ -31,6 +32,19 @@ class Admin::GeneratorController < ApplicationController
     dealers.each do |dealer|
       return dealer if target <= dealer.frequency
       target -= dealer.frequency
+    end
+  end
+  
+  def get_random_time
+    case params[:period]
+    when "1 month"
+      return rand((DateTime.now - 1.months)..DateTime.now)
+    when "3 months"
+      return rand((DateTime.now - 3.months)..DateTime.now)
+    when "1 year"
+      return rand((DateTime.now - 1.years)..DateTime.now)
+    when "3 years"
+      return rand((DateTime.now - 3.years)..DateTime.now)
     end
   end
 
