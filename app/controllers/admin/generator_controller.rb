dd71024b829fe9e3b6994cf11f788edc125a0fa3
HonestBook <lsmwallfacer@gmail.com>
2020-12-17 02:14:11 +0800

    Implement generator with options

--- app/controllers/admin/generator_controller.rb
+++ app/controllers/admin/generator_controller.rb
@@ -1,56 +1,69 @@
 class Admin::GeneratorController < ApplicationController
-  # POST
+  before_action :user_is_admin?
+  before_action :all_params_present?
+
+  # Generates some transactions by the given parameters
   def generate_transactions
-    accounts = params[:account].eql?("All") ? 
-    Account.all : Account.where(:id => params[:account])
-    how_many = params[:how_many].to_i
+    accounts = params[:account].eql?("All") ?
+      Account.all : Account.where(:id => params[:account])
     accounts.each do |account|
       matching_dealers = Dealer.where(:currency => account.currency)
-      for i in 0...how_many do 
+      for i in 0...params[:how_many].to_i
         dealer = get_weighted_random_dealer matching_dealers
-        amount = -(rand(dealer.max_amount) + dealer.min_amount)
-        time = get_random_time
         account.transactions.create!(
-          account: account, 
+          account: account,
           dealer_account_number: dealer.account_number,
           dealer_name: dealer.name,
-          amount: amount,
+          amount: get_random_amount(dealer),
           currency: account.currency,
-          created_at: time
-          );
+          created_at: get_random_time(params[:period]),
+        )
       end
     end
   end
-  
+
   private
 
   # Given an array of dealers.
   # Returns a random dealer weighted by frequency.
-  def get_weighted_random_dealer dealers
-    cum_frequency = dealers.reduce(0) {|sum, dealer| sum + dealer.frequency}
+  def get_weighted_random_dealer(dealers)
+    cum_frequency = dealers.reduce(0) { |sum, dealer| sum + dealer.frequency }
     target = rand(1..cum_frequency)
     dealers.each do |dealer|
       return dealer if target <= dealer.frequency
       target -= dealer.frequency
     end
   end
-  
-  def get_random_time
-    case params[:period]
-    when "1 month"
-      return rand((DateTime.now - 1.months)..DateTime.now)
-    when "3 months"
-      return rand((DateTime.now - 3.months)..DateTime.now)
-    when "1 year"
-      return rand((DateTime.now - 1.years)..DateTime.now)
-    when "3 years"
-      return rand((DateTime.now - 3.years)..DateTime.now)
+
+  # Returns a random amount for a dealer.
+  # If the dealer is a vendor, the amount is always negative.
+  # Else there is a chance that the amount is positive.
+  def get_random_amount(dealer)
+    amount = rand(dealer.max_amount) + dealer.min_amount
+    if dealer.is_vendor
+      amount = -amount
+    else
+      amount = rand > 0.5 ? amount : -amount
     end
+    return amount
   end
 
-  # Given an integer amount.
-  # Returns a float amount with realistic decimal values.
-  def authenticate_amount amount
-    return amount + 0.5;
+  # Returns a random time for the given period option.
+  def get_random_time(option)
+    case option
+    when "1 month"
+      return rand(1.month.ago..Time.now)
+    when "3 months"
+      return rand(3.month.ago..Time.now)
+    when "1 year"
+      return rand(1.year.ago..Time.now)
+    when "3 years"
+      return rand(3.year.ago..Time.now)
+    end
+  end
+  
+  # Check if all parameters are present, if not, redirect to admin accounts page
+  def all_params_present?
+    redirect_to admin_accounts_url unless params[:account] && params[:period] && params[:how_many]
   end
 end
