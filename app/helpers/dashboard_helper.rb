c8ef726e53980a12c5fd464e5e54b430de7db013
HTTOM <k1926372@kcl.ac.uk>
2020-12-16 10:45:25 +0800

    The persoanl dashboard can show the total balance

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,10 @@
 module DashboardHelper
+  def total_balance
+    accounts = Account.where(user: current_user.id)
+    @total_balance = 0
+    accounts.each do|account|
+      @total_balance = account.balance + @total_balance
+    end
+    @total_balance
+  end
 end
