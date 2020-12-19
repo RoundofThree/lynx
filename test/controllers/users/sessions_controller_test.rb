45087502ac66ca00196529a9589fe0259fa501fb
Naissy <helen_hong_atlancia@outlook.com>
2020-12-19 08:29:07 +0800

    add session test template

---
+++ test/controllers/users/sessions_controller_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class Users::SessionControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  # test new
+
+  # test create
+
+  # test destroy
+
+  # test sign-in params
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
