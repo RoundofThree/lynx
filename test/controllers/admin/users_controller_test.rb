7fa178482df5b64c6ad99b0e3e44d1e6b27d34e7
RoundofThree <zhuoyingjiangli@gmail.com>
2020-12-19 15:55:23 +0100

    Remove unnecessary export to xls buttons

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -27,6 +27,13 @@ class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test 'admin user should get users list in xlsx format' do 
+    sign_in users(:admin) 
+    login_as_admin("abc") 
+    get admin_users_url(format: "xlsx")
+    assert_response :success
+  end
+
   test 'admin user should get user details by any user' do
     sign_in users(:admin)
     login_as_admin('abc')
