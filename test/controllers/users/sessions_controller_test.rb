b5e2aa2d4473161e199beb3246d4ee729d35d6a0
RoundofThree <zhuoyingjiangli@gmail.com>
2020-12-19 15:34:54 +0100

    Solve conflicts

--- test/controllers/users/sessions_controller_test.rb
+++ test/controllers/users/sessions_controller_test.rb
@@ -1,30 +1,30 @@
 require 'test_helper'
 
-class Users::SessionControllerTest < ActionDispatch::IntegrationTest
+class SessionsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
-  # test new
+  # new tests 
+  test "should get new" do 
+    get new_user_session_url
+    assert_response :success
+  end 
 
-  # test create
+  # destroy tests
+  test "should sign out" do 
+    sign_in users(:have_no_accounts)
+    delete destroy_user_session_url 
+    assert_redirected_to root_url 
+  end 
 
-  # test destroy
+  # create tests 
+  test "should sign in with a valid user" do
+    post user_session_url, params: { user: { email: "lol1@gmail.com", password: "1234567890"} }
+    assert_redirected_to dashboard_url 
+  end
 
-  # test sign-in params
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  test "should not sign in with an inexisting user" do
+    post user_session_url, params: { user: { email: "hu@qq.com", password: "qwerty" } }
+    assert_template "users/sessions/new"
+  end
 
 end
