58441101da8fbf5cfdde74aec9d85d1ffdd430a8
Naissy <helen_hong_atlancia@outlook.com>
2020-12-21 04:12:46 +0800

    Add charts to admin dashboard

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("chartkick")
+require("chart.js")
 /*
 
 require("@rails/ujs").start()
